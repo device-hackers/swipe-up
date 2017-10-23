dbddc3aa5af72b1b149e0a24e0bbc90fbe84c443
Andriy Halyasovskyy <thebit.hao@gmail.com>
2017-10-23 10:23:03 +0300

    Adding more methods to DOM util (patch)

--- src/utils/dom.js
+++ src/utils/dom.js
@@ -9,36 +9,46 @@ Wrap.prototype.focus = function () {
     if (this.ref) this.ref.focus()
 }
 
-//Basic mimic for https://api.jquery.com/click/
+//Basic mimic for {@link http://api.jquery.com/get/} but returns single element instead of array
+Wrap.prototype.get = function () {
+    if (this.ref) return this.ref
+}
+
+//Basic mimic for {@link https://api.jquery.com/click/}
 Wrap.prototype.click = function (handler) {
     if (this.ref) this.ref.addEventListener('click', handler)
 }
 
-//Basic mimic for http://api.jquery.com/html/
+//Basic mimic for {@link http://api.jquery.com/html/}
 Wrap.prototype.html = function (htmlString) {
     if (this.ref && htmlString) this.ref.innerHTML = htmlString
     else if (this.ref) return this.ref.innerHTML
 }
 
-//Basic mimic for http://api.jquery.com/append/
+//Basic mimic for {@link http://api.jquery.com/append/}
 Wrap.prototype.append = function (content) {
     if (this.ref) {
-        this.ref.innerHTML += content
+        if (content instanceof HTMLElement) {
+            //TODO add impl
+        } else {
+            this.ref.innerHTML += content
+        }
+
         return this.ref
     }
 }
 
-//Basic mimic for http://api.jquery.com/hide/
+//Basic mimic for {@link http://api.jquery.com/hide/}
 Wrap.prototype.hide = function () {
     if (this.ref) this.ref.style.display = 'none'
 }
 
-//Basic mimic for http://api.jquery.com/show/, WARNING! always switches display to block!
+//Basic mimic for {@link http://api.jquery.com/show/}, WARNING! always switches display to block!
 Wrap.prototype.show = function () {
     if (this.ref) this.ref.style.display = 'block'
 }
 
-//Basic mimic for http://api.jquery.com/toggle/
+//Basic mimic for {@link http://api.jquery.com/toggle/}
 Wrap.prototype.toggle = function () {
     if (this.ref) this.ref.style.display === 'none' ? this.show() : this.hide()
 }
