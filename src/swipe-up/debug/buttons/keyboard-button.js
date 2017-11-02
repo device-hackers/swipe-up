b7104d452d517b31a95840994d06e721fa77ebca
Andriy Halyasovskyy <thebit.hao@gmail.com>
2017-11-02 22:29:09 +0200

    Enhancing keyboard button to handle the case when keyboard is hiding with element being focused (close button) gets removed from the DOM - to investigate iOS 11 focus out bug (patch)

--- src/swipe-up/debug/buttons/keyboard-button.js
+++ src/swipe-up/debug/buttons/keyboard-button.js
@@ -4,12 +4,43 @@ import ControlButton from './control-button'
 export default class KeyboardButton extends ControlButton {
     constructor(debugWidget) {
         let selfName = 'keyboard'
-        let input = document.createElement('input')
-        input.setAttribute('class', 'inputForKeyboard')
-        input.setAttribute('tabindex', 0)
-        $('.debugButtons').append(input)
+
+        let createInput = () => {
+            let input = document.createElement('input')
+            input.setAttribute('class', 'inputForKeyboard')
+            input.setAttribute('tabindex', 0)
+            return input
+        }
+
+        let createSelfRemoveButton = () => {
+            let button = document.createElement('button')
+            button.setAttribute('class', 'selfRemove')
+            button.innerHTML = 'X'
+            button.style.width = '1.5em'
+            button.style.flex = 'initial'
+            button.addEventListener('click', () => {
+                $('.selfRemove').get().remove()
+                $('.inputForKeyboard').style.width = '1px'
+            })
+            return button
+        }
+
+        $('.debugButtons').append(createInput())
+
         super(selfName)
 
-        super.click( () => $('.inputForKeyboard').focus() )
+        super.click( () => {
+            let debugButtons = $('.debugButtons').get()
+            let inputForKeyboard = $('.inputForKeyboard').get()
+            let keyboard = $('.keyboard-button').get()
+            let selfRemove = $('.selfRemove').get()
+
+            if (!selfRemove) {
+                debugButtons.insertBefore(createSelfRemoveButton(), keyboard)
+                $('.inputForKeyboard').style.width = '20px'
+            }
+
+            inputForKeyboard.focus()
+        } )
     }
 }
